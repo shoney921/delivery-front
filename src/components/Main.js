766e43c7d6400ff8e3626ecb9fa17070c0a4ebc4
shoney <ddmczp@naver.com>
2023-06-04 17:40:56 +0900

    정렬 방식 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,13 +14,13 @@ function Main() {
   ];
 
   const dropOption2 = [
-    { id: 1, label: "기본 정렬순" },
-    { id: 2, label: "별점 높은순" },
-    { id: 3, label: "리뷰 많은순" },
+    { id: "1", label: "기본 정렬순" },
+    { id: "2", label: "별점 높은순" },
+    { id: "3", label: "리뷰 많은순" },
   ];
 
-  const [selectedOption1, setSelectedOption1] = useState("");
-  const [selectedOption2, setSelectedOption2] = useState(1);
+  const [selectedOption1, setSelectedOption1] = useState("AL");
+  const [selectedOption2, setSelectedOption2] = useState("1");
   const [storeList, setStoreList] = useState([]);
   const [defaultList, setDefaultList] = useState([]);
 
@@ -44,12 +44,17 @@ function Main() {
         `https://63d5f3d5-e29d-4b00-8d40-6be477084fab.mock.pstmn.io/dcx/1/shopList`
       )
       .then((res) => {
-        setStoreList(res.data?.shopList);
-        setDefaultList(res.data?.shopList);
+        setStoreList(sortShop(res.data?.shopList));
+        setDefaultList(sortShop(res.data?.shopList));
       });
   };
 
   useEffect(() => {
+    let newStoreList = newFunction();
+    setStoreList(newStoreList);
+  }, [selectedOption1, selectedOption2]);
+
+  function newFunction() {
     let newStoreList = defaultList.filter((store) => {
       if (selectedOption1 !== "AL") {
         return store.category === selectedOption1;
@@ -57,8 +62,27 @@ function Main() {
         return true;
       }
     });
-    setStoreList(newStoreList);
-  }, [selectedOption1]);
+    newStoreList = sortShop(newStoreList);
+    return newStoreList;
+  }
+
+  function sortShop(inputList) {
+    let newStoreList = Array.from(inputList);
+    if (selectedOption2 === "2") {
+      newStoreList.sort(
+        (x, y) => y.adFlag.localeCompare(x.adFlag) || y.point - x.point
+      );
+    } else if (selectedOption2 === "3") {
+      newStoreList.sort(
+        (x, y) => y.adFlag.localeCompare(x.adFlag) || y.review - x.review
+      );
+    } else if (selectedOption2 === "1") {
+      newStoreList.sort(
+        (x, y) => y.adFlag.localeCompare(x.adFlag) || x.id - y.id
+      );
+    }
+    return newStoreList;
+  }
 
   useEffect(() => {
     getStoreList();
@@ -86,11 +110,20 @@ function Main() {
 
       <div className="Shop-List">
         {storeList.map((store) => (
-          <Link to={`/stores/${store.id}`} key={store.id}>
+          <Link
+            to={`/stores/${store.id}`}
+            key={store.id}
+            style={{ textDecoration: "none" }}
+          >
             <div className="Shop-card">
               <img className="shopImage" src={`/images/${store.imageFile}`} />
               <div>
-                <div className="Store-Name">{store.name}</div>
+                <div className="Store-title">
+                  <div className="Store-Name">{store.name}</div>
+                  {store.adFlag === "Y" && (
+                    <img className="adImage" src="/images/ad.png" />
+                  )}
+                </div>
                 <div>
                   별점 {store.point} , 리뷰 {store.review}
                 </div>
